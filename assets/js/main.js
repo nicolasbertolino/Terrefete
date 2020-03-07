f52f16fd87314fe0e23601324cc26004446c2c8d
Nicolas Bertolino <bertolinonicolas@gmail.com>
2020-03-07 22:35:27 +0100

    Add animations on landing

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,15 +18,43 @@ function topFunction() {
 const cursor = document.querySelector(".cursor");
 
 document.addEventListener("mousemove", e => {
-  cursor.setAttribute("style", "top: " + (e.pageY - 17) + "px; left: " + (e.pageX - 17) + "px;")
+  cursor.setAttribute(
+    "style",
+    "top: " + (e.pageY - 17) + "px; left: " + (e.pageX - 17) + "px;"
+  );
 });
 
-document.addEventListener("wheel", e => {
-  cursor.setAttribute("style", "top: " + (e.pageY - 17) + "px; left: " + (e.pageX - 17) + "px;")
-});
-
-document.querySelectorAll("a").forEach((link) => {
+document.querySelectorAll("a").forEach(link => {
   link.addEventListener("mouseenter", () => cursor.classList.add("condense"));
-  link.addEventListener("mouseleave", () => cursor.classList.remove("condense"));
+  link.addEventListener("mouseleave", () =>
+    cursor.classList.remove("condense")
+  );
 });
 
+// Intro GSAP
+const element = document.querySelector("h1");
+charming(element);
+
+gsap.from("h1 span", {
+  duration: 1,
+  delay: 0.5,
+  y: 30,
+  opacity: 0,
+  stagger: 0.05,
+  ease: "power4"
+});
+
+gsap.from(".title h4", {
+  duration: 1,
+  delay: 1,
+  y: 30,
+  opacity: 0,
+  ease: "power4"
+});
+
+gsap.from(".scroll", {
+  duration: 1,
+  delay: 1.3,
+  opacity: 0,
+  ease: "power4"
+});
